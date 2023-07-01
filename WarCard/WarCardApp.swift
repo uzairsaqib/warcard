373a60b684927b7e579b9ac6f4e758c83bc874c1
Uzair Saqib <uzairsaqib110@gmail.com>
2023-07-01 11:35:25 -0400

    Initial Commit

---
+++ WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by Uzair Saqib on 2023-07-01.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
